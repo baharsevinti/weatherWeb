594c13f1e827b188f6dee8d6ffa386da9c1cbe56
BİLİ <121384275+baharsevinti@users.noreply.github.com>
2025-04-23 12:01:57 +0300

    weather fetch operation added

--- script/tahmin.js
+++ script/tahmin.js
@@ -1,14 +1,28 @@
 const key ='P9r9AYnFr4LPi1PPacFPCOcuharHLulu';
+const havaDurumuGetir=async (Id)=>{
+    const temelUrl='http://dataservice.accuweather.com/currentconditions/v1/';
+    const sorgu=`${Id}?apikey=${key}`;
+    const res= await fetch(temelUrl+sorgu);
+    const veri= await res.json();
+    
+}
+
 const sehirGetir=async(konum)=>{
     const temelUrl='http://dataservice.accuweather.com/locations/v1/cities/search';
     const sorgu=`?apikey=${key}&q=${konum}`;
     const res= await fetch(temelUrl+sorgu);
     const veri= await res.json();
     console.log(veri);
+    return veri[0]; // burada return ekleniyor
 };
-sehirGetir('adana').then((veri)=>{
-    console.log(veri);
-}
 
-);
+sehirGetir('adana').then((veri)=>{
+   
+    return havaDurumuGetir(veri.Key);
+}).then((veri)=>{
+    console.log(veri);
+}).catch((hata)=>{
+    console.log('Hata:', hata);
+});
+
 
